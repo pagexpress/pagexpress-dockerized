672e08da76025deb165d48c9ae013deee6e2612b
Maciej Krzemianowski <dev@jmzmedia.eu>
2021-08-13 12:52:53 +0200

    update: Replace staging setup with local

--- startup-db.sh
+++ startup-db.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Copy local backup
-docker-compose -f docker-compose-dev.yml exec -T px_staging_mongo rm -rf /dump
-docker-compose -f docker-compose-dev.yml exec -T px_staging_mongo mkdir /dump
-docker cp data/dump.gz $(docker ps -aqf "name=px_staging_mongo"):/dump/
+sudo docker-compose -f docker-compose-dev.yml exec -T px_dev_mongo rm -rf /dump
+sudo docker-compose -f docker-compose-dev.yml exec -T px_dev_mongo mkdir /dump
+sudo docker cp data/dump.gz $(sudo docker ps -aqf "name=px_dev_mongo"):/dump/
 
 # Run mongorestore using last dump
-# Use these options to migrate to different db: --nsFrom="local.*" --nsTo="staging.*"
-docker-compose -f docker-compose-dev.yml exec -T px_staging_mongo mongorestore --uri="mongodb://root:root@0.0.0.0:27017/local?authSource=admin" --gzip --archive="/dump/dump.gz" --drop
+# Use these options to migrate to different db: --nsFrom="local.*" --nsTo="dev.*"
+sudo docker-compose -f docker-compose-dev.yml exec -T px_dev_mongo mongorestore --uri="mongodb://root:root@0.0.0.0:27017/local?authSource=admin" --gzip --archive="/dump/dump.gz" --drop
